5106c2ac965dbdece8801373b12eb90b778c75ad
abigaillilley <abigaillilley@gmail.com>
2019-02-19 12:06:27 +0000

    added Niha's midi work

---
+++ src/guitarMIDI.java
@@ -0,0 +1,79 @@
+import java.io.File;
+import javax.sound.midi.Instrument;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Synthesizer;
+import javax.sound.midi.Track;
+
+public class guitarMIDI {
+    final static String FILE = "MamaDo.mid";
+
+    public static String instrumentName( int n ) {
+        try {
+            final Synthesizer synth = MidiSystem.getSynthesizer();
+            synth.open();
+            final Instrument[] instrs = synth.getAvailableInstruments();
+            synth.close();
+            return instrs[ n ].getName();
+        } catch ( Exception exn ) {
+            System.out.println( exn ); System.exit( 1 ); return "";
+        }
+    }
+
+    public static String noteName( int n ) {
+        final String[] NAMES =
+                { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+        final int octave = (n / 12) - 1;
+        final int note   = n % 12;
+        return NAMES[ note ] + octave;
+    }
+
+    public static void displayTrack( Track trk ) {
+
+        for ( int i = 0; i < trk.size(); i = i + 1 ) {
+            MidiEvent   evt  = trk.get( i );
+            MidiMessage msg = evt.getMessage();
+            if ( msg instanceof ShortMessage ) {
+                final long         tick = evt.getTick();
+                final ShortMessage smsg = (ShortMessage) msg;
+                final int          cmd  = smsg.getCommand();
+                final int          dat1 = smsg.getData1();
+                    switch (cmd) {
+                        case ShortMessage.PROGRAM_CHANGE:
+                            if(instrumentName(dat1).equals("Clean Gt.")) {
+                                System.out.print("@" + tick + ", ");
+                                System.out.println("Program change: " + instrumentName(dat1));
+                            }
+                            break;
+                        case ShortMessage.NOTE_ON:
+                            System.out.print("@" + tick + ", ");
+                            System.out.println("Note on:  " + dat1);
+                            break;
+                        default:
+                            /* ignore other commands */
+                            break;
+                    }
+            }
+        }
+    }
+    public static void displaySequence( Sequence seq ) {
+        Track[] trks = seq.getTracks();
+
+        for ( int i = 0; i < trks.length; i++ ) {
+            System.out.println( "Track " + i );
+            displayTrack( trks[ i ] );
+        }
+    }
+
+    public static void main( String[] argv ) {
+        try {
+            Sequence seq = MidiSystem.getSequence( new File( FILE ) );
+            displaySequence( seq );
+        } catch ( Exception exn ) {
+            System.out.println( exn ); System.exit( 1 );
+        }
+    }
+}

c7e42dffa692ec0e4295fd9ea2ab0ce5aaf71c63
niha12 <ng342@exeter.ac.uk>
2019-03-01 17:58:27 +0000

    Tried to fix meta things

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module GSE.Project {
     requires javafx.fxml;
     requires javafx.controls;
     requires java.xml;
+    requires junit;
+    requires java.desktop;
 
     opens org.gsep.select to javafx.fxml;
     exports org.gsep.select;

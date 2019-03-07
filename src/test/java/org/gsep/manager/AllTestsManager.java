3d32706c4b50c1b0884e0655928bc5d099fa0e9e
Humzah Malik <humzahmalik@hotmail.com>
2019-03-07 22:25:39 +0000

    Commit before örs merge

--- src/test/java/org/gsep/manager/AllTestsManager.java
+++ src/test/java/org/gsep/manager/AllTestsManager.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ StoreManagerFrameTest.class, SongTest.class })
-public class AllTests {
+public class AllTestsManager {
 	
 
 }

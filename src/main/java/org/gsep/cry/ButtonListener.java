ba6da7c1f367775b971b2c003ca365e5b82f8638
abigaillilley <abigaillilley@gmail.com>
2019-03-07 21:57:58 +0000

    DONE FOR SPRINT 2

---
+++ src/main/java/org/gsep/cry/ButtonListener.java
@@ -0,0 +1,19 @@
+package org.gsep.cry;
+
+/**
+ * ButtonListener interface.
+ *
+ * @author  Abigail Lilley
+ * @version 1.0, March 2019.
+ */
+public interface ButtonListener
+{
+    /**
+     * This method will react and handle events that occur to the Buttons
+     * being listened to by an implementation of ButtonListener.
+     * @param buttonName assigned name of the button to make the code more readable and intuitive.
+     *                   Implementations process the event depending on the button, identified by this name.
+     * @param event event triggered. The Button's state can be found from this.
+     */
+    public void stateReceived( String buttonName, ButtonEvent event );
+}
---
+++ src/main/java/org/gsep/cry/Note.java
@@ -0,0 +1,11 @@
+package org.gsep.cry;
+
+/**
+ * ButtonListener interface.
+ *
+ * @author  Örs Barkanyi
+ * @version 1.0, February 2019.
+ */
+public enum Note{
+    OPEN, BLACK, WHITE;
+}

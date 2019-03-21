e96bdc3a778772578b49747905992fdcbdb6b5d0
abigaillilley <abigaillilley@gmail.com>
2019-03-21 20:02:41 +0000

    unix number correct

--- src/main/java/org/gsep/ButtonNumbers.java
+++ src/main/java/org/gsep/ButtonNumbers.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public enum ButtonNumbers {
     MACNUMBERS     (new int[]{ 0, 1, 4, 2, 5, 3, 8, 15, 10, 12, 13, 17}),
-    UNIXNUMBERS    (new int[]{ 0, 1, 4, 2, 5, 3, 8, 16, 10, 12, 13, 14}),
+    UNIXNUMBERS    (new int[]{ 0, 1, 4, 2, 5, 3, 8, 14, 10, 12, 17, 16}),
     WINDOWSNUMBERS (new int[]{ 0, 1, 4, 2, 5, 3, 8, 16, 10, 12, 13, 14});
 
     private final int[] numbers;

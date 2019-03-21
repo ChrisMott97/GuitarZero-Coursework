a3ec1b3e917d2a9c4430d4dd9cc85e5bc2155937
abigaillilley <abigaillilley@gmail.com>
2019-03-21 22:39:54 +0000

    Revert "discard (pray4me)"
    This reverts commit 3b64908db43b3d7b759c8d3204741446eb3b8713.

---
+++ src/main/java/org/gsep/ButtonNumbers.java
@@ -0,0 +1,20 @@
+package org.gsep;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public enum ButtonNumbers {
+    MACNUMBERS     (new int[]{ 0, 1, 4, 2, 5, 3, 8, 15, 10, 12, 13, 17}),
+    UNIXNUMBERS    (new int[]{ 0, 1, 4, 2, 5, 3, 8, 14, 10, 12, 17, 16}),
+    WINDOWSNUMBERS (new int[]{ 0, 1, 4, 2, 5, 3, 8, 16, 10, 12, 13, 14});
+
+    private final int[] numbers;
+
+    ButtonNumbers(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+}

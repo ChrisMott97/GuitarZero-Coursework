9c970d894cf8e6357953154adbad311da439c7eb
abigaillilley <abigaillilley@gmail.com>
2019-03-21 22:37:28 +0000

    comment out linkguitar

--- src/main/java/org/gsep/mediator/SceneModule.java
+++ src/main/java/org/gsep/mediator/SceneModule.java
@@ -78,92 +78,92 @@ public abstract class SceneModule {
 
     public void init(){}
 
-    public void linkGuitar( SceneController controller) {
-        System.out.println("Linking Guitar");
-        ControllerEnvironment cenv = ControllerEnvironment.getDefaultEnvironment();
-        Controller[] ctrls = cenv.getControllers();
-        int[] buttonNumbers;
-
-        try {
-            String osName = System.getProperty("os.name");
-            if (osName == null) {
-                throw new IOException("os.name not found");
-            }
-            osName = osName.toLowerCase(Locale.ENGLISH);
-            if (osName.contains("windows")) {
-                buttonNumbers = ButtonNumbers.WINDOWSNUMBERS.getNumbers();
-            } else if (osName.contains("linux")
-                    || osName.contains("mpe/ix")
-                    || osName.contains("freebsd")
-                    || osName.contains("irix")
-                    || osName.contains("digital unix")
-                    || osName.contains("unix")) {
-                buttonNumbers = ButtonNumbers.UNIXNUMBERS.getNumbers();
-            } else if (osName.contains("mac os")) {
-                buttonNumbers = ButtonNumbers.MACNUMBERS.getNumbers();
-                System.out.println("THIS IS A MAC");
-            } else {
-                throw new IOException("os.name not supported");
-            }
-
-            Button[] buttons = new Button[ buttonNumbers.length ];
-            for ( int i = 0; i < buttons.length; i = i + 1 ) {
-                buttons[ i ] = new Button( ButtonNames.BUTTONNAMES.getNames()[i], buttonNumbers[i]);
-                buttons[ i ].addButtonListener( controller );	/* Adding listeners to Buttons depending on the mode */
-                Thread buttonThread = new Thread(buttons[ i ]);
-                buttonThread.start();								            /* Starting a thread for each Button */
-            }
-
-        } catch (IOException ex) {
-            System.out.println("OS not identified, can't run game");
-            ex.getMessage();
-            ex.printStackTrace();
-            //TODO terminate game
-        }
-    }
-
-    public void linkGuitar( NoteHighwayController controller) {
-        System.out.println("Linking Guitar");
-        ControllerEnvironment cenv = ControllerEnvironment.getDefaultEnvironment();
-        Controller[] ctrls = cenv.getControllers();
-        GuitarEventHandler guitarEventHandler = new GuitarEventHandler(controller);
-        int[] buttonNumbers;
-
-        try {
-            String osName = System.getProperty("os.name");
-            if (osName == null) {
-                throw new IOException("os.name not found");
-            }
-            osName = osName.toLowerCase(Locale.ENGLISH);
-            if (osName.contains("windows")) {
-                buttonNumbers = ButtonNumbers.WINDOWSNUMBERS.getNumbers();
-            } else if (osName.contains("linux")
-                    || osName.contains("mpe/ix")
-                    || osName.contains("freebsd")
-                    || osName.contains("irix")
-                    || osName.contains("digital unix")
-                    || osName.contains("unix")) {
-                buttonNumbers = ButtonNumbers.UNIXNUMBERS.getNumbers();
-            } else if (osName.contains("mac os")) {
-                buttonNumbers = ButtonNumbers.MACNUMBERS.getNumbers();
-                System.out.println("THIS IS A MAC");
-            } else {
-                throw new IOException("os.name not supported");
-            }
-
-            Button[] buttons = new Button[ buttonNumbers.length ];
-            for ( int i = 0; i < buttons.length; i = i + 1 ) {
-                buttons[ i ] = new Button( ButtonNames.BUTTONNAMES.getNames()[i], buttonNumbers[i]);
-                buttons[ i ].addButtonListener( guitarEventHandler );	/* Adding listeners to Buttons depending on the mode */
-                Thread buttonThread = new Thread(buttons[ i ]);
-                buttonThread.start();								            /* Starting a thread for each Button */
-            }
-
-        } catch (IOException ex) {
-            System.out.println("OS not identified, can't run game");
-            ex.getMessage();
-            ex.printStackTrace();
-            //TODO terminate game
-        }
-    }
+//    public void linkGuitar( SceneController controller) {
+//        System.out.println("Linking Guitar");
+//        ControllerEnvironment cenv = ControllerEnvironment.getDefaultEnvironment();
+//        Controller[] ctrls = cenv.getControllers();
+//        int[] buttonNumbers;
+//
+//        try {
+//            String osName = System.getProperty("os.name");
+//            if (osName == null) {
+//                throw new IOException("os.name not found");
+//            }
+//            osName = osName.toLowerCase(Locale.ENGLISH);
+//            if (osName.contains("windows")) {
+//                buttonNumbers = ButtonNumbers.WINDOWSNUMBERS.getNumbers();
+//            } else if (osName.contains("linux")
+//                    || osName.contains("mpe/ix")
+//                    || osName.contains("freebsd")
+//                    || osName.contains("irix")
+//                    || osName.contains("digital unix")
+//                    || osName.contains("unix")) {
+//                buttonNumbers = ButtonNumbers.UNIXNUMBERS.getNumbers();
+//            } else if (osName.contains("mac os")) {
+//                buttonNumbers = ButtonNumbers.MACNUMBERS.getNumbers();
+//                System.out.println("THIS IS A MAC");
+//            } else {
+//                throw new IOException("os.name not supported");
+//            }
+//
+//            Button[] buttons = new Button[ buttonNumbers.length ];
+//            for ( int i = 0; i < buttons.length; i = i + 1 ) {
+//                buttons[ i ] = new Button( ButtonNames.BUTTONNAMES.getNames()[i], buttonNumbers[i]);
+//                buttons[ i ].addButtonListener( controller );	/* Adding listeners to Buttons depending on the mode */
+//                Thread buttonThread = new Thread(buttons[ i ]);
+//                buttonThread.start();								            /* Starting a thread for each Button */
+//            }
+//
+//        } catch (IOException ex) {
+//            System.out.println("OS not identified, can't run game");
+//            ex.getMessage();
+//            ex.printStackTrace();
+//            //TODO terminate game
+//        }
+//    }
+//
+//    public void linkGuitar( NoteHighwayController controller) {
+//        System.out.println("Linking Guitar");
+//        ControllerEnvironment cenv = ControllerEnvironment.getDefaultEnvironment();
+//        Controller[] ctrls = cenv.getControllers();
+//        GuitarEventHandler guitarEventHandler = new GuitarEventHandler(controller);
+//        int[] buttonNumbers;
+//
+//        try {
+//            String osName = System.getProperty("os.name");
+//            if (osName == null) {
+//                throw new IOException("os.name not found");
+//            }
+//            osName = osName.toLowerCase(Locale.ENGLISH);
+//            if (osName.contains("windows")) {
+//                buttonNumbers = ButtonNumbers.WINDOWSNUMBERS.getNumbers();
+//            } else if (osName.contains("linux")
+//                    || osName.contains("mpe/ix")
+//                    || osName.contains("freebsd")
+//                    || osName.contains("irix")
+//                    || osName.contains("digital unix")
+//                    || osName.contains("unix")) {
+//                buttonNumbers = ButtonNumbers.UNIXNUMBERS.getNumbers();
+//            } else if (osName.contains("mac os")) {
+//                buttonNumbers = ButtonNumbers.MACNUMBERS.getNumbers();
+//                System.out.println("THIS IS A MAC");
+//            } else {
+//                throw new IOException("os.name not supported");
+//            }
+//
+//            Button[] buttons = new Button[ buttonNumbers.length ];
+//            for ( int i = 0; i < buttons.length; i = i + 1 ) {
+//                buttons[ i ] = new Button( ButtonNames.BUTTONNAMES.getNames()[i], buttonNumbers[i]);
+//                buttons[ i ].addButtonListener( guitarEventHandler );	/* Adding listeners to Buttons depending on the mode */
+//                Thread buttonThread = new Thread(buttons[ i ]);
+//                buttonThread.start();								            /* Starting a thread for each Button */
+//            }
+//
+//        } catch (IOException ex) {
+//            System.out.println("OS not identified, can't run game");
+//            ex.getMessage();
+//            ex.printStackTrace();
+//            //TODO terminate game
+//        }
+//    }
 }

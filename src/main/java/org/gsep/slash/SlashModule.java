ac8b98d5c6b4fa7377866d1cfc248dde83f3c3ff
abigaillilley <abigaillilley@gmail.com>
2019-03-21 14:20:18 +0000

    remove system.exit if no controller found move linkGuitar

--- src/main/java/org/gsep/slash/SlashModule.java
+++ src/main/java/org/gsep/slash/SlashModule.java
@@ -27,11 +27,7 @@ public class SlashModule extends SceneModule {
 
     private static SlashModule instance;
 
-    private final static String[] BUTTONNAMES = {   "zeroPower",
-                                                "strumBar",
-                                                "escape"    };
     private SlashModule(){
-        linkGuitar();
     }
 
     public static SlashModule getInstance(){
@@ -57,56 +53,6 @@ public class SlashModule extends SceneModule {
             System.out.println("Slash controller could not load.");
         }
         setTitle("Slash Mode");
-        linkGuitar();
-    }
-
-    private void linkGuitar() {
-        System.out.println("Linking Guitar");
-        ControllerEnvironment cenv = ControllerEnvironment.getDefaultEnvironment();
-        Controller[] ctrls = cenv.getControllers();
-        int[] buttonNums;
-        final int[] windowsButtonNums = {8, 15, 10};
-        final int[] unixButtonNums    = { 0, 1, 4};
-        final int[] macButtonNums = { 8, 15, 10 };
-        //TODO get values for different OS
-
-
-
-        try {
-            String osName = System.getProperty("os.name");
-            if (osName == null) {
-                throw new IOException("os.name not found");
-            }
-            osName = osName.toLowerCase(Locale.ENGLISH);
-            if (osName.contains("windows")) {
-                buttonNums = windowsButtonNums;
-            } else if (osName.contains("linux")
-                    || osName.contains("mpe/ix")
-                    || osName.contains("freebsd")
-                    || osName.contains("irix")
-                    || osName.contains("digital unix")
-                    || osName.contains("unix")) {
-                buttonNums = unixButtonNums;
-            } else if (osName.contains("mac os")) {
-                buttonNums = macButtonNums;
-                System.out.println("THIS IS A MAC");
-            } else {
-                throw new IOException("os.name not supported");
-            }
-
-            Button[] buttons = new Button[ BUTTONNAMES.length ];
-            for ( int i = 0; i < buttons.length; i = i + 1 ) {
-                buttons[ i ] = new Button( BUTTONNAMES[i], buttonNums[i]);
-                buttons[ i ].addButtonListener( controller );			/* Adding listeners to Buttons depending on the mode */
-                Thread buttonThread = new Thread(buttons[ i ]);
-                buttonThread.start();								/* Starting a thread for each Button */
-            }
-
-        } catch (IOException ex) {
-            System.out.println("OS not identified, can't run game");
-            ex.getMessage();
-            ex.printStackTrace();
-            //TODO terminate game
-        }
+        linkGuitar(controller);
     }
 }
